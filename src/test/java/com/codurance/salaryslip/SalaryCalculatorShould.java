f5e5a2fdee06064d20040ba68fb9e2d108a2ab68
nawhi <25394965+nawhi@users.noreply.github.com>
2019-02-12 10:16:46 +0000

    Use MethodSource in parameterized test

--- src/test/java/com/codurance/salaryslip/SalaryCalculatorShould.java
+++ src/test/java/com/codurance/salaryslip/SalaryCalculatorShould.java
@@ -1,36 +1,46 @@
 package com.codurance.salaryslip;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SalaryCalculatorShould {
 
     @ParameterizedTest
-    @CsvSource({
-            "3000,250",
-            "1500,125",
-            "5000,416.67"
-    })
-    void calculate_gross_salary_per_month_for_an_employee(int annualSalary, double expectedMonthlySalary) {
-        SalaryCalculator calculator = new SalaryCalculator(new Money(annualSalary));
+    @MethodSource("grossSalaryData")
+    void calculate_gross_salary_per_month_for_an_employee(Money annualSalary, Money expectedMonthlySalary) {
+        SalaryCalculator calculator = new SalaryCalculator(annualSalary);
 
         Money actualMonthlySalary = calculator.calculateGrossMonthlySalary();
 
-        assertEquals(new Money(expectedMonthlySalary), actualMonthlySalary);
+        assertEquals(expectedMonthlySalary, actualMonthlySalary);
     }
 
+    private static Stream<Arguments> grossSalaryData() {
+        return Stream.of(
+                Arguments.of(new Money(3000),new Money(250)),
+                Arguments.of(new Money(1500),new Money(125)),
+                Arguments.of(new Money(5000),new Money(416.67))
+        );
+    }
     @ParameterizedTest
-    @CsvSource({
-            "8424,0",
-            "20424, 1439.94",
-    })
-    void calculate_national_insurance_contributions_per_month_for_a_annual_salary(int annualSalary, double expectedNationalInsuranceContribution) {
-        SalaryCalculator calculator = new SalaryCalculator(new Money(annualSalary));
+    @MethodSource("niContributionData")
+    void calculate_national_insurance_contributions_per_month_for_a_annual_salary(Money annualSalary, Money expectedNationalInsuranceContribution) {
+        SalaryCalculator calculator = new SalaryCalculator(annualSalary);
 
         Money actualNationalInsuranceContribution = calculator.calculateNationalInsuranceContribution();
 
-        assertEquals(new Money(expectedNationalInsuranceContribution), actualNationalInsuranceContribution);
+        assertEquals(expectedNationalInsuranceContribution, actualNationalInsuranceContribution);
+    }
+
+    private static Stream<Arguments> niContributionData() {
+        return Stream.of(
+                Arguments.of(new Money(8424),new Money(0)),
+                Arguments.of(new Money(20424),new Money(1439.94))
+        );
     }
 }
