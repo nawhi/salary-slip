25ab7b9d62c46426f0da98c52e385d85c252cc9f
nawhi <25394965+nawhi@users.noreply.github.com>
2019-02-12 12:28:07 +0000

    Use a parameterized test for NI calculator

--- src/main/java/com/codurance/salaryslip/NationalInsuranceCalculator.java
+++ src/main/java/com/codurance/salaryslip/NationalInsuranceCalculator.java
@@ -1,7 +1,7 @@
 package com.codurance.salaryslip;
 
 class NationalInsuranceCalculator {
-    static Money calculate(Money salary, Money lowerBound, double v) {
+    static Money calculate(Money salary, Money lowerBound, double rate) {
         if (salary.isGreaterThan(lowerBound))
             return new Money(100);
         return new Money(0);
--- src/test/java/com/codurance/salaryslip/NationalInsuranceCalculatorShould.java
+++ src/test/java/com/codurance/salaryslip/NationalInsuranceCalculatorShould.java
@@ -1,6 +1,11 @@
 package com.codurance.salaryslip;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,19 +14,26 @@ class NationalInsuranceCalculatorShould {
     @Test
     void return_zero_for_incomes_under_the_minimum_threshold() {
         Money contribution = NationalInsuranceCalculator.calculate(
-                new Money(5000), // ANNUAL salary
-                new Money(6000),  // ANNUAL contribution lower bound
+                new Money(5000),
+                new Money(6000),
                 0.1);
         assertEquals(new Money(0), contribution);
     }
 
-    @Test
-    void return_lower_rate_contribution_for_salary_above_lower_bound() {
+    @ParameterizedTest
+    @MethodSource("lowerRateContribData")
+    void return_lower_rate_contribution_for_salary_above_lower_bound(Money salary, Money lowerBound, double rate, Money expectedContribution) {
         Money contribution = NationalInsuranceCalculator.calculate(
-                new Money(3000), // salary
-                new Money(2000), // lower bound
-                0.1 // lower rate
+                salary,
+                lowerBound,
+                rate
         );
-        assertEquals(new Money(100), contribution);
+        assertEquals(expectedContribution, contribution);
+    }
+
+    static Stream<Arguments> lowerRateContribData() {
+        return Stream.of(
+                Arguments.of(new Money(3000), new Money(2000), 0.1, new Money(100))
+        );
     }
 }
