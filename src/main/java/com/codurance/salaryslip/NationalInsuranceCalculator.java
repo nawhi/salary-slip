c34b4cef4ab8430e615d467f1d43b674f8e62dd9
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-02-12 13:59:07 +0000

    Extracted functions to improve intent of NationalInsuranceCalculator.

--- src/main/java/com/codurance/salaryslip/NationalInsuranceCalculator.java
+++ src/main/java/com/codurance/salaryslip/NationalInsuranceCalculator.java
@@ -2,10 +2,19 @@ package com.codurance.salaryslip;
 
 class NationalInsuranceCalculator {
     static Money calculate(Money salary, Money lowerBound, double lowerRate, Money upperBound, double upperRate) {
-        if (salary.isGreaterThan(upperBound))
-            return upperBound.subtract(lowerBound).multiply(lowerRate).add((salary).subtract(upperBound).multiply(upperRate));
-        if (salary.isGreaterThan(lowerBound))
-            return salary.subtract(lowerBound).multiply(lowerRate);
+        if (isAboveNIBoundary(salary, upperBound))
+            return calculateBoundaryContributions(upperBound, lowerBound,lowerRate).add(calculateBoundaryContributions(salary, upperBound, upperRate));
+        if (isAboveNIBoundary(salary, lowerBound)) {
+            return calculateBoundaryContributions(salary, lowerBound, lowerRate);
+        }
         return new Money(0);
+    }
+
+    private static Money calculateBoundaryContributions(Money salary, Money lowerBound, double lowerRate) {
+        return salary.subtract(lowerBound).multiply(lowerRate);
+    }
+
+    private static boolean isAboveNIBoundary(Money salary, Money upperBound) {
+        return salary.isGreaterThan(upperBound);
     }
 }
