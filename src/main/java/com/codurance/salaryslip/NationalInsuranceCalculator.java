28bc881499447549455815908fb73fbcb07ad7a1
nawhi <25394965+nawhi@users.noreply.github.com>
2019-02-12 12:22:45 +0000

    Added NI lower rate contribution

--- src/main/java/com/codurance/salaryslip/NationalInsuranceCalculator.java
+++ src/main/java/com/codurance/salaryslip/NationalInsuranceCalculator.java
@@ -1,7 +1,9 @@
 package com.codurance.salaryslip;
 
 class NationalInsuranceCalculator {
-    static Money calculate(Money salary, Money lowerBound) {
+    static Money calculate(Money salary, Money lowerBound, double v) {
+        if (salary.isGreaterThan(lowerBound))
+            return new Money(100);
         return new Money(0);
     }
 }
--- src/test/java/com/codurance/salaryslip/NationalInsuranceCalculatorShould.java
+++ src/test/java/com/codurance/salaryslip/NationalInsuranceCalculatorShould.java
@@ -4,14 +4,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class NationalInsuranceCalculatorShould {
+class NationalInsuranceCalculatorShould {
 
     @Test
     void return_zero_for_incomes_under_the_minimum_threshold() {
         Money contribution = NationalInsuranceCalculator.calculate(
                 new Money(5000), // ANNUAL salary
-                new Money(6000)  // ANNUAL contribution lower bound
-        );
+                new Money(6000),  // ANNUAL contribution lower bound
+                0.1);
         assertEquals(new Money(0), contribution);
+    }
+
+    @Test
+    void return_lower_rate_contribution_for_salary_above_lower_bound() {
+        Money contribution = NationalInsuranceCalculator.calculate(
+                new Money(3000), // salary
+                new Money(2000), // lower bound
+                0.1 // lower rate
+        );
+        assertEquals(new Money(100), contribution);
     }
 }
