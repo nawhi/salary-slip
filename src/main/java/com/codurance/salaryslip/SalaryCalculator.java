50c4176f8bea5a2cfe0ab28b155d7a5c3e1c80b8
nawhi <25394965+nawhi@users.noreply.github.com>
2019-02-12 10:17:19 +0000

    Extract method with NI parameters

--- src/main/java/com/codurance/salaryslip/SalaryCalculator.java
+++ src/main/java/com/codurance/salaryslip/SalaryCalculator.java
@@ -13,10 +13,12 @@ class SalaryCalculator {
     }
 
     Money calculateNationalInsuranceContribution() {
-        Money lowerBandThresholdPerWeek = new Money(162.01);
+        return calculateNationalInsuranceContribution(new Money(162.01), 0.12);
+    }
+
+    Money calculateNationalInsuranceContribution(Money lowerBandThresholdPerWeek, double lowerBandNIRate) {
         Money lowerBandThresholdAnnual = lowerBandThresholdPerWeek.multiply(WEEKS_IN_A_YEAR);
         if(annualSalary.subtract(lowerBandThresholdAnnual).isPositive()) {
-            double lowerBandNIRate = 0.12;
             return annualSalary.subtract(lowerBandThresholdAnnual).multiply(lowerBandNIRate);
         }
         return new Money(0);
