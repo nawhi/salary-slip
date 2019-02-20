d21c44dca15af7f12002369fc291288f2efc557f
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-02-20 11:23:54 +0000

    Fixed AT_NationalInsuraceContribution to use correct values.

--- src/test/java/com/codurance/salaryslip/AT_NationalInsuranceContribution.java
+++ src/test/java/com/codurance/salaryslip/AT_NationalInsuranceContribution.java
@@ -8,13 +8,18 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AT_NationalInsuranceContribution {
-    @Disabled
     @Test
     void generate_monthly_slip_for_basic_pay_with_national_insurance_contributions() {
-        Employee employee = new Employee("John J Doe", 12345, new Money(9060.00));
+        Money annualSalary = new Money(8476.00);
+        int id = 12345;
+        String john_j_doe = "John J Doe";
+        Employee employee = new Employee(john_j_doe, id, annualSalary);
         SalarySlipGenerator generator = new SalarySlipGenerator();
         SalarySlip generatedSlip = generator.generateFor(employee);
-        SalarySlip expectedSlip = new SalarySlip(employee, new Money(755.00), new Money(10));
+
+        Money expectedNiContributions = new Money(6.24);
+        Money expectedMonthlyGrossSalary = new Money(706.33);
+        SalarySlip expectedSlip = new SalarySlip(employee, expectedMonthlyGrossSalary, expectedNiContributions);
 
         assertEquals(expectedSlip, generatedSlip);
     }
