fc703a64404ff7b06a20f312996e72fdebf991ea
Solange U. Gasengayire <sgasengayire@gmail.com>
2019-03-08 11:40:36 +0000

    Extract constants

--- src/main/java/com/codurance/salaryslip/SalarySlipGenerator.java
+++ src/main/java/com/codurance/salaryslip/SalarySlipGenerator.java
@@ -5,20 +5,19 @@ import com.codurance.salaryslip.calculators.GrossMonthlySalaryCalculator;
 
 class SalarySlipGenerator {
 
-    public static final Money UPPER_BOUND_THRESHOLD = new Money(892).multiply(52);
-    public static final double UPPER_RATE = 0.02;
+    private static final Money UPPER_BOUND_THRESHOLD = new Money(892).multiply(52);
+    private static final double UPPER_RATE = 0.02;
+    private static final Money LOWER_BOUND_THRESHOLD = new Money(162 * 52);
+    private static final double LOWER_RATE = 0.12;
 
     SalarySlip generateFor(Employee employee) {
         Money annualSalary = employee.getAnnualSalary();
 
         Money monthlyGrossSalary = new GrossMonthlySalaryCalculator(annualSalary).calculateGrossMonthlySalary();
 
-        Money weeklyLowerBoundThreshold = new Money(162);
-        double lowerBandNIRate = 0.12;
-
         NationalInsuranceCalculator calculator = new NationalInsuranceCalculator(
-                weeklyLowerBoundThreshold.multiply(52),
-                lowerBandNIRate,
+                LOWER_BOUND_THRESHOLD,
+                LOWER_RATE,
                 UPPER_BOUND_THRESHOLD,
                 UPPER_RATE);
 
