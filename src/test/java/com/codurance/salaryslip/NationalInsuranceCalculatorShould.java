472b19ddfb0a9cb83942deacd2cf53e670f71da4
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-03-08 11:15:20 +0000

    Refactor tests to use now instance method.

--- src/test/java/com/codurance/salaryslip/NationalInsuranceCalculatorShould.java
+++ src/test/java/com/codurance/salaryslip/NationalInsuranceCalculatorShould.java
@@ -14,21 +14,24 @@ class NationalInsuranceCalculatorShould {
 
     @Test
     void return_zero_for_incomes_under_the_minimum_threshold() {
-        Money contribution = NationalInsuranceCalculator.calculate(
-                new Money(5000),
-                new Money(6000),
-                0.1, new Money(7000), 0.01);
+        NationalInsuranceCalculator nationalInsuranceCalculator = new NationalInsuranceCalculator(
+                new Money(6000), 0.1,
+                new Money(7000), 0.01
+        );
+        Money contribution = nationalInsuranceCalculator.calculationFunction(new Money(5000));
         assertEquals(new Money(0), contribution);
     }
 
     @ParameterizedTest
     @MethodSource("lowerRateContribData")
     void return_lower_rate_contribution_for_salary_above_lower_bound(Money salary, Money lowerBound, double rate, Money expectedContribution) {
-        Money contribution = NationalInsuranceCalculator.calculate(
-                salary,
+        NationalInsuranceCalculator nationalInsuranceCalculator = new NationalInsuranceCalculator(
                 lowerBound,
                 rate,
-                new Money(10000), 0.01);
+                new Money(10000),
+                0.01
+        );
+        Money contribution = nationalInsuranceCalculator.calculationFunction(salary);
         assertEquals(expectedContribution, contribution);
     }
 
@@ -50,8 +53,14 @@ class NationalInsuranceCalculatorShould {
         double lowerRate = 0.1;
         Money upperBound = new Money(9000);
         double upperRate = 0.01;
-        Money contribution = NationalInsuranceCalculator.calculate(salary, lowerBound, lowerRate, upperBound, upperRate);
+        NationalInsuranceCalculator nationalInsuranceCalculator = new NationalInsuranceCalculator(
+                lowerBound,
+                lowerRate,
+                upperBound,
+                upperRate
+        );
+        Money contributionTemp = nationalInsuranceCalculator.calculationFunction(salary);
         double contributeCalculated = 4000 *0.1 + 1000 * 0.01;
-        assertEquals(new Money(contributeCalculated), contribution);
+        assertEquals(new Money(contributeCalculated), contributionTemp);
     }
 }
