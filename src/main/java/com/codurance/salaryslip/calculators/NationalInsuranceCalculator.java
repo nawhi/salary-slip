9798e95111fdd75f5cfc008ed55e8c0bb50d8a1b
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-03-08 11:03:11 +0000

    Introduce instance method to prepare to remove static implementation.

--- src/main/java/com/codurance/salaryslip/calculators/NationalInsuranceCalculator.java
+++ src/main/java/com/codurance/salaryslip/calculators/NationalInsuranceCalculator.java
@@ -3,7 +3,25 @@ package com.codurance.salaryslip.calculators;
 import com.codurance.salaryslip.Money;
 
 public class NationalInsuranceCalculator {
+    private final Money lowerBound;
+    private final double lowerRate;
+    private final Money upperBound;
+    private final double upperRate;
+
+    public NationalInsuranceCalculator(Money lowerBound, double lowerRate, Money upperBound, double upperRate) {
+        this.lowerBound = lowerBound;
+        this.lowerRate = lowerRate;
+        this.upperBound = upperBound;
+        this.upperRate = upperRate;
+    }
+
     public static Money calculate(Money salary, Money lowerBound, double lowerRate, Money upperBound, double upperRate) {
+        NationalInsuranceCalculator niCalc = new NationalInsuranceCalculator(lowerBound, lowerRate, upperBound,
+                upperRate);
+        return niCalc.calculationFunction(salary);
+    }
+
+    public Money calculationFunction(Money salary) {
         if (isAboveNIBoundary(salary, upperBound)){
             return calculateBoundaryContributions(upperBound, lowerBound,lowerRate)
                     .add(calculateBoundaryContributions(salary, upperBound, upperRate));
